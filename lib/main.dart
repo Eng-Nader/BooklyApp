738131983bba3a8b26e75a5bc4513fbe181a4fef
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-11 15:17:24 +0300

    change in test name app

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ void main() {
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
