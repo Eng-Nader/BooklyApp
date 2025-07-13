f36e2b6f69190b22a4a0270785f047f7ad2ec3d1
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-14 01:05:37 +0300

    create Style File and title HomeView

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const titleMedium = TextStyle(
+    color: Colors.white,
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+  );
+}
