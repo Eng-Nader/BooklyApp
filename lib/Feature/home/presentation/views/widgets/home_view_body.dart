e0ee8014d4d0d5a8088e13131a379dedfdf2bd4d
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-14 05:42:40 +0300

    refactor for CustomScroolView

--- lib/Feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/Feature/home/presentation/views/widgets/book_info.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/book_info_view.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/book_view.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_app_bar.dart';
@@ -15,25 +14,22 @@ class HomeViewBody extends StatelessWidget {
       child: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-            child: CustomAppBar(),
-          ),
-          SliverToBoxAdapter(
-            child: BookCardView(),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 50,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Text(
-              'Best Seller',
-              style: Styles.textStyle18,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                BookCardView(),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
           ),
           BookInfoView(),
