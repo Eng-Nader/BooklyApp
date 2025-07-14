62dc09e8445418926a45546da58fb43bbc087021
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-14 03:06:51 +0300

    finish AppStyle

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -6,5 +7,25 @@ abstract class Styles {
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
-  
+  static const textStyle14 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle30 = TextStyle(
+    color: Colors.white,
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle20 = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtfont,
+  );
 }
