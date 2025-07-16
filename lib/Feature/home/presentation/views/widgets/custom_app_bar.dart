820c645a6fbfe642251ee95141805f29147c94dd
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-16 08:07:57 +0300

    create a search View and route

--- lib/Feature/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Feature/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -20,7 +22,9 @@ class CustomAppBar extends StatelessWidget {
             AssetsData.klogo,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.ksearchView);
+            },
             icon: const Icon(
               color: Colors.white,
               size: 32,
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:bookly_app/Feature/Splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/Feature/home/presentation/views/home_details.dart';
 import 'package:bookly_app/Feature/home/presentation/views/home_view.dart';
+import 'package:bookly_app/Feature/search/presentation/view/search_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const khomeView = '/homeView';
   static const khomeDetails = '/homeDetails';
+  static const ksearchView = '/SearchView';
 
   static final router = GoRouter(
     routes: [
@@ -26,6 +28,12 @@ abstract class AppRouter {
           );
         },
       ),
+      GoRoute(
+        path: ksearchView,
+        pageBuilder: (context, state) {
+          return const CupertinoPage(child: SearchView());
+        },
+      )
     ],
   );
 }
