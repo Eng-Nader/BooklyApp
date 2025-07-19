57e3a9675df63db6fcf74d481c5ab6561e804556
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-20 01:39:43 +0300

    Edit in bookCardCubit

--- lib/Feature/home/presentation/manger/book_card_cubit/book_card_cubit.dart
+++ lib/Feature/home/presentation/manger/book_card_cubit/book_card_cubit.dart
@@ -1,7 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Feature/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Feature/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'book_card_state.dart';
 
@@ -16,8 +17,8 @@ class BookCardCubit extends Cubit<BookCardState> {
     var result = await homeRepo.fetchBookCard();
     result.fold((failure) {
       emit(BookCardFailureState(errorMessage: failure.errorMessage));
-    }, (sucess) {
-      emit(BookCardSucessState(booksList: sucess));
+    }, (booksCard) {
+      emit(BookCardSucessState(booksList: booksCard));
     });
   }
 }
