9dd15acef608b80822f1c1b09e39b143af719cac
NaderSameer <nadersameer@std.mans.edu.eg>
2025-07-20 10:09:17 +0300

    add setUp method to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:bookly_app/Feature/home/data/repos/home_repo_implement.dart';
 import 'package:bookly_app/Feature/home/presentation/manger/book_card_cubit/book_card_cubit.dart';
 import 'package:bookly_app/Feature/home/presentation/manger/book_info_cubit/book_info_cubit.dart';
-import 'package:bookly_app/Feature/home/presentation/views/widgets/book_card.dart';
 import 'package:bookly_app/constant.dart';
-import 'package:bookly_app/core/utils/api_servers.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/servers_locator.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setUp();
   runApp(
     const BooklyApp(),
   );
